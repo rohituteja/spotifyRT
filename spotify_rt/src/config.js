6fe6b8a65be2c13b46c72835f2159496d5a462b4
rohituteja <rtutejajr@gmail.com>
2021-10-03 18:26:26 -0400

    Update localhost redirect

--- spotify_rt/src/config.js
+++ spotify_rt/src/config.js
@@ -2,7 +2,7 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "e3caab180ca840f98ef77b0e05646808";
-export const redirectUri = "http://localhost:3000/";
+export const redirectUri = "https://rohituteja.github.io/spotifyRT";
 export const scopes = [
     "user-read-private",
     "user-top-read",
